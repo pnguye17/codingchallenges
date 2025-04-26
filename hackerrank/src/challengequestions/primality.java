97f5594d2b1c76c013a274bfd6e26a13bfc796fa
pnguye17 <peter.nguyen17@gmail.com>
2025-04-26 00:28:53 -0700

    done

---
+++ hackerrank/src/challengequestions/primality.java
@@ -0,0 +1,27 @@
+package challengequestions;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class primality {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        BigInteger ans = new BigInteger(num);
+
+
+        if (ans.isProbablePrime(0)){
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+        
+
+       
+        sc.close();
+
+    }
+
+}
