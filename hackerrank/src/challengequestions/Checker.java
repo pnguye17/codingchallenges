ebddf56e918764603a6ce19c02225206be641a98
pnguye17 <peter.nguyen17@gmail.com>
2025-04-29 15:41:06 -0700

    need to override compare method

---
+++ hackerrank/src/challengequestions/Checker.java
@@ -0,0 +1,17 @@
+package challengequestions;
+
+import java.util.Comparator;
+
+public class Checker<T> implements Comparator<T>{
+
+    @Override
+    public int compare(T o1, T o2) {
+    
+        // compare score of the player object and sort them 
+
+        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+    }
+
+    
+
+}
