1655187907274c035156fcf866e8846d2ca8d591
pnguye17 <peter.nguyen17@gmail.com>
2025-04-28 17:59:32 -0700

    switch to ecp wrote prelim code for hashmap with objects

--- hackerrank/src/challengequestions/JavaMapProblem.java
+++ hackerrank/src/challengequestions/JavaMapProblem.java
@@ -1,5 +1,32 @@
 package challengequestions;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class JavaMapProblem {
 
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int numOfQuery = sc.nextInt();
+    	
+    	Map<String, String> phoneBook = new HashMap<>();
+    	
+    	for (int i = 0; i < numOfQuery; i++) {
+    		String name = sc.nextLine();
+    		String phone = sc.nextLine();
+    		phoneBook.put(name, phone);
+    	}
+    	
+    	sc.close();
+    	
+    	phoneBook.forEach((k,v) -> System.out.printf(
+    			k.toString(),v.toString()
+    			));
+    	
+    	
+   
+    }
 }
