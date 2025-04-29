eb9569768c27f79ad07d575f0d313a23a5e15131
pnguye17 <peter.nguyen17@gmail.com>
2025-04-29 00:01:18 -0700

    javasort

---
+++ hackerrank/src/challengequestions/JavaSort.java
@@ -0,0 +1,17 @@
+package challengequestions;
+
+public class JavaSort {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+class Student {
+    private Integer id;
+    private String name;
+    private Float cgpa;
+    
+    
+}
