83df6d9971517a28245c78c6e36f24b9fb0e9493
pnguye17 <peter.nguyen17@gmail.com>
2025-04-29 15:20:08 -0700

    done generic

--- hackerrank/src/challengequestions/Printer.java
+++ hackerrank/src/challengequestions/Printer.java
@@ -1,5 +1,11 @@
 package challengequestions;
 
-public class Printer {
+public class Printer<T> {
+
+    T objT;
+
+    public <T> void printArray(){
+        System.out.println(this.objT.toString());
+    }
 
 }
