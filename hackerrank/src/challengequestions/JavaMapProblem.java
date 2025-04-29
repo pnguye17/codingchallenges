28deaa3335e9a70281064dee11501db32b8bcf34
pnguye17 <peter.nguyen17@gmail.com>
2025-04-28 23:40:56 -0700

    completed hashMap

--- hackerrank/src/challengequestions/JavaMapProblem.java
+++ hackerrank/src/challengequestions/JavaMapProblem.java
@@ -7,25 +7,32 @@ import java.util.Scanner;
 public class JavaMapProblem {
 
     public static void main(String[] args) {
+
+		//add Person obj to Map
     	
     	Scanner sc = new Scanner(System.in);
-    	
-    	int numOfQuery = sc.nextInt();
+    	int entry = sc.nextInt();
     	
     	Map<String, String> phoneBook = new HashMap<>();
-    	
-    	for (int i = 0; i < numOfQuery; i++) {
-			sc.next();
-    		String name = sc.nextLine();
+		
+		sc.nextLine();
+    	for (int i = 1; i <= entry; i++) {
+			String name = sc.nextLine().toLowerCase();
     		String phone = sc.nextLine();
     		phoneBook.put(name, phone);
     	}
     	
+		while (entry >= 1) {
+			String search = sc.nextLine().toLowerCase();
+			if (phoneBook.containsKey(search)) {
+				System.out.println(search+ "=" + phoneBook.get(search));
+			} else {
+				System.out.println("Not found");
+			}
+			entry--;
+		}
     	sc.close();
-    	
-		System.out.println(phoneBook.toString());
-    	
-    	
-   
+
+       
     }
 }
